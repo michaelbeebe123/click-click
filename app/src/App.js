c334a39b202abc11bb674535a8cf35b01176590e
michaelbeebe123 <beebenmichael@gmail.com>
2019-12-28 22:22:13 -0500

    rewriting app.js functions

--- app/src/App.js
+++ app/src/App.js
@@ -14,36 +14,14 @@ class App extends Component {
       highscore: 0
   };
 
-  gameOver = () => {
-    if (this.state.score > this.state.highscore) {
-      this.setState({highscore: this.state.score}, function() {
-        console.log(this.state.highscore);
-      });
-    }
-    this.state.cards.forEach(card => {
-      card.count = 0;
-    });
-    alert(`Game Over. \nscore: ${this.state.score}`);
-    this.setState({score: 0});
-    return true;
+  // TODO:
+  clickCount = () => {
+    
   }
 
-  clickCount = () => {
-    this.state.cards.find((x, i) => {
-      if (x.id === cards.id) {
-        if(cards[i].count === 0){
-          cards[i].count = cards[i].count + 1;
-          this.setState({score : this.state.score + 1}, function(){
-            console.log(this.state.score);
-          });
-          this.state.cards.sort(() => Math.random() - 0.5)
-          return true; 
-        } 
-        else {
-          this.gameOver();
-        }
-      }
-    });
+  // TODO:
+  gameOver = () => {
+    
   }
 
   render() {
