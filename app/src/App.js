e7eacdf49845dc356a80eba3fc20beae45c26c7e
michaelbeebe123 <beebenmichael@gmail.com>
2019-12-28 22:19:30 -0500

    added another card to make the grid even

--- app/src/App.js
+++ app/src/App.js
@@ -62,6 +62,7 @@ class App extends Component {
                 />
               ))}
             </div>
+            <br></br>
           {/* </Draggable>
         </Droppable> */}
     </Wrapper>
