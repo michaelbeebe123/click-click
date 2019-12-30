e814e5ed7aea5cea1b94184f8b89f1442ced6aa3
michaelbeebe123 <beebenmichael@gmail.com>
2019-12-29 20:47:11 -0500

    fixed duplicate props in Header.js

--- app/src/components/Header/Header.js
+++ app/src/components/Header/Header.js
@@ -5,13 +5,13 @@ const Header = props => (
     <div className="jumbotron jumbotron-fluid">
         <div className="container">
             <div className="row">
-                <div className="title" className="col-sm">
+                <div className="col-sm">
                     <h1>Dunder Mifflin 
                         <br></br>Clicky Game</h1>
                     <br></br>
                     <p>Try to click each image without clicking one twice</p>
                 </div>
-                <div className="score" className="col-sm">
+                <div className="col-sm">
                     <h3>Score: {props.score}</h3>
                     <br></br>
                     <h3>Highscore: {props.highScore}</h3>
